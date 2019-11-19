0c93a491868d537cff887f446805926ee3d12e5c
Jaid <jaid.jsx@gmail.com>
2019-11-19 18:55:42 +0100

    Added error logging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import {getInput} from "@actions/core"
+import {getInput, setFailed} from "@actions/core"
 import {which} from "@actions/io"
 import {exec} from "@actions/exec"
 
 async function main() {
   const nodeEnv = getInput("nodeEnv")
   const npmPath = await which("npm", true)
-  await exec(npmPath, "install", {
+  const exitCode = await exec(npmPath, "install", {
     env: {
       NODE_ENV: nodeEnv,
     },
   })
+  if (!exitCode !== 0) {
+    setFailed(`Installing failed with code ${exitCode}`)
+    return
+  }
 }
 
 main()
