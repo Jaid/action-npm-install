7dd665be72527eedabbd144691abbbbf05d469e5
Jaid <jaid.jsx@gmail.com>
2019-11-19 17:28:30 +0100

    Added input `nodeEnv`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import {getInput} from "@actions/core"
 import {which} from "@actions/io"
 import {exec} from "@actions/exec"
 
 async function main() {
+  const nodeEnv = getInput("nodeEnv")
   const npmPath = await which("npm", true)
   await exec(npmPath, "install", {
     env: {
-      NODE_ENV: "development",
+      NODE_ENV: nodeEnv,
     },
   })
 }
