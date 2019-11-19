3b39c00b9cb1ece12c02b32bb8b2497985c8ae0f
Jaid <jaid.jsx@gmail.com>
2019-11-19 20:17:26 +0100

    Fixed check
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import {exec} from "@actions/exec"
 async function main() {
   const nodeEnv = getInput("nodeEnv")
   const npmPath = await which("npm", true)
-  const exitCode = await exec(npmPath, "install", {
-    env: {
-      NODE_ENV: nodeEnv,
-    },
-  })
-  if (!exitCode !== 0) {
+  const exitCode = await exec(npmPath, "install")
+  if (exitCode !== 0) {
     setFailed(`Installing failed with code ${exitCode}`)
     return
   }
