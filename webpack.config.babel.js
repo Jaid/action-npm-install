c7eb51daa55adfb950925b157f0edc7cb4919034
Jaid <jaid.jsx@gmail.com>
2019-11-29 10:43:48 +0100

    manage: Changed to `webpack-config-jaid` type `githubAction`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,3 @@
-import path from "path"
-
 import configure from "webpack-config-jaid"
 
-export default configure({
-  type: "nodeScript",
-  include: false,
-  nodeExternals: false,
-  outDir: path.join(__dirname, "build"),
-  licenseFileName: false,
-})
+export default configure()
